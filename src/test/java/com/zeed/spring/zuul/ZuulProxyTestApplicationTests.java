32219408eb0f0f8b289d29a6f3de778303df3c9c
zeedt <yusufsaheedtaiwo@gmail.com>
2020-10-06 16:19:08 +0100

    first commit

---
+++ src/test/java/com/zeed/spring/zuul/ZuulProxyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeed.spring.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulProxyTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
